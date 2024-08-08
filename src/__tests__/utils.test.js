40108669dce3769fe512be09f7efa5f7c3a695c4
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-08-08 23:43:18 +0300

    initial commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("returns true for a palindrome word", () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  it("returns false for a non-palindrome word", () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 // Your code here
+export function isPalindrome(word) {
+    const reversedWord = word.split("").reverse().join("");
+    return word === reversedWord;
+  }
+  
